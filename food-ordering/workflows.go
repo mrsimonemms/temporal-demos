ddced2920baf9e9b07209a8c6db050c184454b35
Simon Emms <simon@simonemms.com>
2025-07-24 21:00:24 +0000

    feat(food-ordering): create workflow with basic configuration

--- food-ordering/workflows.go
+++ food-ordering/workflows.go
@@ -15,3 +15,120 @@
  */
 
 package foodordering
+
+import (
+	"fmt"
+	"slices"
+	"time"
+
+	"go.temporal.io/sdk/workflow"
+)
+
+// @todo(sje): automatically cancel inactive workflows
+func OrderWorkflow(ctx workflow.Context, state OrderState) error {
+	logger := workflow.GetLogger(ctx)
+
+	// Query to return status of basket
+	if err := workflow.SetQueryHandler(ctx, Queries.GET_STATUS, func(_ []byte) (OrderState, error) {
+		logger.Debug("Returning order status")
+		return state, nil
+	}); err != nil {
+		logger.Error("SetQueryHandler failed.", "error", err, "query", Queries.GET_STATUS)
+		return err
+	}
+
+	var a *activities
+
+	// Add a new item into the basket
+	if err := workflow.SetUpdateHandlerWithOptions(
+		ctx,
+		Updates.ADD_ITEM,
+		func(ctx workflow.Context, item OrderProduct) error {
+			logger.Info("Adding new item to basket", "productId", item.ProductID, "quantity", item.Quantity)
+			state.AddItem(item)
+
+			return nil
+		},
+		workflow.UpdateHandlerOptions{
+			Validator: func(ctx workflow.Context, item OrderProduct) error {
+				key := slices.IndexFunc(allProducts, func(i Product) bool {
+					return i.ProductID == item.ProductID
+				})
+
+				if key == -1 {
+					return fmt.Errorf("unknown product id")
+				}
+
+				if item.Quantity <= 0 {
+					return fmt.Errorf("quantity must be minimum of 1")
+				}
+
+				return nil
+			},
+		},
+	); err != nil {
+		logger.Error("SetUpdateHandlerWithOptions failed.", "Error", err, "update", Updates.ADD_ITEM)
+		return err
+	}
+
+	// Remove an item from the basket
+	if err := workflow.SetUpdateHandler(
+		ctx,
+		Updates.REMOVE_ITEM,
+		func(ctx workflow.Context, item OrderProduct) error {
+			logger.Info("Removing item from the basket", "productId", item.ProductID, "quantity", item.Quantity)
+			state.RemoveItem(item)
+
+			return nil
+		},
+	); err != nil {
+		logger.Error("SetUpdateHandlerWithOptions failed.", "Error", err, "update", Updates.REMOVE_ITEM)
+		return err
+	}
+
+	// Update the order status - this will come from the restaurant
+	if err := workflow.SetUpdateHandlerWithOptions(
+		ctx,
+		Updates.UPDATE_STATUS,
+		func(ctx workflow.Context, input string) error {
+			status, _ := ParseOrderStatus(input)
+
+			logger.Info("Updating order status", "status", status)
+			state.Status = status
+
+			return nil
+		},
+		workflow.UpdateHandlerOptions{
+			Validator: func(ctx workflow.Context, input string) error {
+				if _, err := ParseOrderStatus(input); err != nil {
+					logger.Debug("Invalid status", "input", input)
+					return err
+				}
+
+				return nil
+			},
+		},
+	); err != nil {
+		logger.Error("SetUpdateHandlerWithOptions failed.", "Error", err, "update", Updates.UPDATE_STATUS)
+		return err
+	}
+
+	// Wait for the checkout signal
+	_ = workflow.GetSignalChannel(ctx, Signals.CHECKOUT).Receive(ctx, nil)
+
+	logger.Info("Checkout triggered")
+
+	// Change state to pending
+	state.Status = OrderStatusPending
+
+	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		StartToCloseTimeout: time.Minute,
+	})
+
+	if err := workflow.ExecuteActivity(ctx, a.DoSomething).Get(ctx, nil); err != nil {
+		logger.Error("Error checking out", "error", err)
+		return fmt.Errorf("error checking out: %w", err)
+	}
+
+	return nil
+}
