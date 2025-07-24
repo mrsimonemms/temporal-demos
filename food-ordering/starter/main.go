e2902a4251250c3c4d5d56147851ea10e403a1d6
Simon Emms <simon@simonemms.com>
2025-07-24 17:43:40 +0100

    feat(food-ordering): initial commit

---
+++ food-ordering/starter/main.go
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"go.temporal.io/sdk/client"
+)
+
+func main() {
+	// The client is a heavyweight object that should be created once per process.
+	c, err := client.Dial(client.Options{
+		HostPort: os.Getenv("TEMPORAL_ADDRESS"),
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	// workflowOptions := client.StartWorkflowOptions{
+	// 	TaskQueue: "payments",
+	// }
+
+	// ctx := context.Background()
+
+	// we, err := c.ExecuteWorkflow(ctx, workflowOptions, schedulepayments.FindDuePaymentsWorkflow)
+	// if err != nil {
+	// 	log.Fatalln("Unable to execute workflow", err)
+	// }
+
+	// log.Println("Started workflow", "WorkflowID", we.GetID(), "RunID", we.GetRunID())
+
+	// // Synchronously wait for the workflow completion.
+	// if err := we.Get(ctx, nil); err != nil {
+	// 	log.Fatalln("Unable get workflow result", err)
+	// }
+
+	log.Println("Triggered")
+}
